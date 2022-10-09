125909c315501a82718f63794b8dbe76ea461ba0
EvgenyKreger <evgeny_kreger@mail.ru>
2022-10-09 17:15:58 +0300

    fix: change text

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 app.get('/',(req:Request,res:Response)=>{
-    res.send('Hello world !!!')
+    res.send('Hello Node!!!!!')
 })
 app.listen(port,()=>{
     console.log(`Port: ${port} is working`)
