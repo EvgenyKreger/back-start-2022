ba1d35139bdb2ebd34587167ccc6a2acad05179f
EvgenyKreger <evgeny_kreger@mail.ru>
2022-10-15 17:10:16 +0300

    feat: add requests Ref: null

--- src/middleware/validateVideos.ts
+++ src/middleware/validateVideos.ts
@@ -2,7 +2,7 @@ import {Request, Response} from "express";
 import {VideoUpdateIdModels, VideoUpdateModel} from "../models/videoModels";
 export const validateCreateVideosMiddleWare = (req: Request, res: Response, next: () => void) => {
     const body = req.body
-    if (!body.title.trim() || body.title.length > 40 ||
+    if (!body.title || !body.title.trim() || body.title.length > 40 || !body.author ||
         !body.author.trim() || body.author.length > 20 ||
         !body.availableResolutions) {
         res.status(400).send({
@@ -22,9 +22,10 @@ export const validateCreateVideosMiddleWare = (req: Request, res: Response, next
 
 export const validateUpdateVideosMiddleWare = (req: Request<VideoUpdateIdModels,VideoUpdateModel>, res: Response, next: () => void) => {
     const body = req.body
-    if (!body.title.trim() || body.title.length > 40 ||
-        !body.author.trim() || body.author.length > 20 ||
-        body.availableResolutions.length === 0 || typeof (body.canBeDownloaded) !== 'boolean' ||
+    if (!body.title  || !body.title.trim() || body.title.length > 40 || !body.author ||
+        !body.author.trim() || body.author.length > 20 || !body.availableResolutions || !body.canBeDownloaded ||
+        !body.minAgeRestriction || !body.publicationDate ||
+        body.availableResolutions.length === 0 || !body.canBeDownloaded || typeof (body.canBeDownloaded) !== 'boolean' ||
         typeof (body.minAgeRestriction) !== 'number' || body.minAgeRestriction < 1 || body.minAgeRestriction > 18 ||
         typeof (body.publicationDate) !== 'string') {
         res.status(400).send({
