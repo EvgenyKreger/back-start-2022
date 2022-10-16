9a4f7167a8f96f6bb7ad5414f0f0f6738bb0968f
EvgenyKreger <evgeny_kreger@mail.ru>
2022-10-16 13:59:51 +0300

    feat: add requests Ref: null

--- src/middleware/validateVideos.ts
+++ src/middleware/validateVideos.ts
@@ -3,40 +3,111 @@ import {VideoUpdateIdModels, VideoUpdateModel} from "../models/videoModels";
 
 export const validateCreateVideosMiddleWare = (req: Request, res: Response, next: () => void) => {
     const body = req.body
-    if (!body.title || !body.title.trim() || body.title.length > 40 || !body.author ||
-        !body.author.trim() || body.author.length > 20 ||
-        !body.availableResolutions) {
+    if (!body.title || !body.title.trim() || body.title.length > 40) {
         res.status(400).send({
             errorsMessages: [
                 {
-                    message: "string",
-                    field: "string",
+                    message: "maxLength: 40",
+                    field: "title",
                 }
             ]
         })
-        return;
+        return
+    }
+    if (!body.author || !body.author.trim() || body.author.length > 20) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "maxLength: 20",
+                    field: "author",
+                }
+            ]
+        })
+        return
+    }
+    if (!body.availableResolutions) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "At least one resolution should be added",
+                    field: "availableResolutions",
+                }
+            ]
+        })
+        return
     } else {
         next()
     }
 }
 export const validateUpdateVideosMiddleWare = (req: Request<VideoUpdateIdModels, VideoUpdateModel>, res: Response, next: () => void) => {
     const body = req.body
-    if (!body.title || !body.title.trim() || body.title.length > 40 || !body.author ||
-        !body.author.trim() || body.author.length > 20 || !body.availableResolutions || !body.canBeDownloaded ||
-        !body.publicationDate ||
-        body.availableResolutions.length === 0 || !body.canBeDownloaded || typeof (body.canBeDownloaded) !== 'boolean' ||
-        typeof (body.minAgeRestriction) !== 'number' || body.minAgeRestriction < 1 || body.minAgeRestriction > 18 ||
-        typeof (body.publicationDate) !== 'string') {
+    if (!body.title || !body.title.trim() || body.title.length > 40 ) {
         res.status(400).send({
             errorsMessages: [
                 {
-                    message: "string",
-                    field: "string",
+                    message: "maxLength: 40",
+                    field: "title",
                 }
             ]
         })
         return;
-    } else {
+    }
+    if (!body.author || !body.author.trim() || body.author.length > 20) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "maxLength: 20",
+                    field: "author",
+                }
+            ]
+        })
+        return;
+    }
+    if (!body.availableResolutions || body.availableResolutions.length === 0) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "At least one resolution should be added",
+                    field: "availableResolutions",
+                }
+            ]
+        })
+        return;
+    }
+    if (!body.canBeDownloaded ||  typeof (body.canBeDownloaded) !== 'boolean') {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "At least one canBeDownloaded should be added",
+                    field: "canBeDownloaded",
+                }
+            ]
+        })
+        return;
+    }
+    if (!body.publicationDate || typeof (body.publicationDate) !== 'string') {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "string",
+                    field: "publicationDate",
+                }
+            ]
+        })
+        return;
+    }
+    if (typeof (body.minAgeRestriction) !== 'number' || body.minAgeRestriction < 1 || body.minAgeRestriction > 18) {
+        res.status(400).send({
+            errorsMessages: [
+                {
+                    message: "min 1 max 18",
+                    field: "minAgeRestriction",
+                }
+            ]
+        })
+        return;
+    }
+    else {
         next()
     }
 }
